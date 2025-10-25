caaa2948c0e5d7b5faf5a0dbd0ee77d42bf0a27c
Alex SHD <alex@shdlabs.com>
2025-10-25 12:45:23 +0300

    Added version flag, shows dev by default or current git tag

--- internal/cmd/root.go
+++ internal/cmd/root.go
@@ -1,13 +1,19 @@
 package cmd
 
 import (
+	"fmt"
+
 	"github.com/spf13/cobra"
 )
 
+// Version is set via -ldflags at build time
+var Version = "dev"
+
 var rootCmd = &cobra.Command{
-	Use:   "proj",
-	Short: "Project scaffolding tool",
-	Long:  `proj helps you quickly create new projects with proper structure and boilerplate code.`,
+	Use:     "proj",
+	Short:   "Project scaffolding tool",
+	Long:    `proj helps you quickly create new projects with proper structure and boilerplate code.`,
+	Version: Version,
 }
 
 func Execute() error {
@@ -15,5 +21,6 @@ func Execute() error {
 }
 
 func init() {
-	// Add commands here
+	// Customize version template to show just the version
+	rootCmd.SetVersionTemplate(fmt.Sprintf("proj version %s\n", Version))
 }
